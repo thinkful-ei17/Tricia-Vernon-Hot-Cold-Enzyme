c86aec22469ca47b5cbec442905896302613f00f
TriciaF <TriciaF@thinkful.com>
2018-01-26 12:09:45 -0500

    added props test for Feedback component

--- src/components/feedback.test.js
+++ src/components/feedback.test.js
@@ -4,7 +4,13 @@ import { shallow } from 'enzyme';
 import './setupTests';
 
 describe('<Feedback/>', () => {
-    it('Should smoke test the Feedback component', () => {
-        shallow( < Feedback / > )
-    })
-})
+  it('Should smoke test the Feedback component', () => {
+    shallow( < Feedback /> );
+    });
+  
+  it('Should test the Feedback props', () => {
+    const key = '64';
+    const wrapper = shallow(<Feedback key={key} />);
+    expect(wrapper.contains(<h2>{key}</h2>));
+    });
+});
