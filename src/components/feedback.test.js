0ed26fe67ab1d50ba76579167c63cb6aba9c0134
TriciaF <TriciaF@thinkful.com>
2018-01-26 11:18:59 -0500

    add tests for feedback and guess-count components

---
+++ src/components/feedback.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Feedback from './feedback';
+import { shallow } from 'enzyme';
+import './setupTests';
+
+describe('<Feedback/>', () => {
+    it('Should smoke test the Feedback component', () => {
+        shallow( < Feedback / > )
+    })
+})
---
+++ src/components/guess-count.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import GuessCount from './guess-count';
+import { shallow } from 'enzyme';
+import './setupTests';
+
+describe('<GuessCount/>', () => {
+    it('Should Smoke test the guess-count component', () => {
+        shallow( < GuessCount / > )
+    })
+})
