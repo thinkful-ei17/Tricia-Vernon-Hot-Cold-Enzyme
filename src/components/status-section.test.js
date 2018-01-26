1beaace05ae3206b43368ce6d8e0bf29042b6e95
Vernon Mensah <vernonmensah@gmail.com>
2018-01-26 12:41:27 -0500

    added tests for status section!

--- src/components/status-section.test.js
+++ src/components/status-section.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import StatusSection from './status-section';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import './setupTests';
 
 describe('<StatusSection/>', () => {
     it('Should smoke test the StatusSection component', () => {
-        // shallow( <StatusSection / > )
+        shallow( <StatusSection guesses={[]}/ > )
+    })
+
+    it('Should show given prop values when rendered', () => {
+        const wrapper = mount(<StatusSection guesses={[1,2,3]} auralStatus='Won!'/ >)
+        // console.log('Here!');
+        // console.log(wrapper.props().auralStatus);
+        expect(wrapper.props().auralStatus).toEqual('Won!');
+        expect(wrapper.prop('guesses')).toEqual([1,2,3]);
     })
 })
